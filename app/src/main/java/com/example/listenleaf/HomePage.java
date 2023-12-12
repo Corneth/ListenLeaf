79fc6c9f91bbd56202544b06433bd16cdca332f1
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-12 02:54:18 -0500

    added home page background and made change in manifest so app runs alone

--- app/src/main/java/com/example/listenleaf/HomePage.java
+++ app/src/main/java/com/example/listenleaf/HomePage.java
@@ -2,13 +2,34 @@ package com.example.listenleaf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class HomePage extends AppCompatActivity {
+    VideoView videoBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        videoBackground = findViewById(R.id.homevideoView);
+        videoBackground.setMediaController(null);
+        videoBackground.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.forestmountainview);
+        videoBackground.start();
+        videoBackground.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
+
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        // to restart the video after coming from other activity
+        videoBackground.start();
     }
 }
