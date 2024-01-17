c1f7fe632274ed83fdaac14417a7cf18ea33129b
jess bacchi <135135190+cheecsey@users.noreply.github.com>
2024-01-17 15:26:33 -0500

    i created a mock books about page that currently has the about of Lord of the Flies. as well as, have added a couple font files for use, and a green leaf background for the books about page.

---
+++ app/src/main/java/com/example/listenleaf/BooksAbout.java
@@ -0,0 +1,14 @@
+package com.example.listenleaf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BooksAbout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books_about);
+    }
+}
