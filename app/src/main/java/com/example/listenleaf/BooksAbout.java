87c963ce17e56ef4f87d64f8ccaa6f1612f14f1d
jess bacchi <135135190+cheecsey@users.noreply.github.com>
2024-01-31 22:07:14 -0500

    books about page: added a back button (needs to be linked), title, author and summary are attached to strings that also need to be linked to the book that was clicked on. changed the font to make it more legible with the lettering and spacing.

--- app/src/main/java/com/example/listenleaf/BooksAbout.java
+++ app/src/main/java/com/example/listenleaf/BooksAbout.java
@@ -3,12 +3,33 @@ package com.example.listenleaf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BooksAbout extends AppCompatActivity {
 
+    String _title;
+    String _author;
+    String _summary;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books_about);
+
+        TextView title = findViewById(R.id.title);
+        title.setText(_title);
+        TextView author = findViewById(R.id.author);
+        author.setText(_author);
+        TextView summary = findViewById(R.id.summary);
+        summary.setText(_summary);
+
+        Button backbtn = findViewById(R.id.button2);
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
